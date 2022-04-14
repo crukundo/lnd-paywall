e9404c7cd591e9cfe7642ef2256c7e83f44c379a
Collin Rukundo <collin@matchstick.ug>
2022-04-15 00:11:40 +0300

    Update

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-# from apps.blog.models import Article
+from apps.blog.models import Article
 
 
 # @admin.register(Article)
 # class ArticleAdmin(admin.ModelAdmin):
 #     list_display = ("title", "user", "status")
 #     list_filter = ("user", "status", "timestamp")
+
+
+admin.site.register(Article)
--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from apps.payments.models import Payment
 # Register your models here.
+
+admin.site.register(Payment)
